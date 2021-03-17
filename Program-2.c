7ef4c879f87f1eb2ec9177c26ef50cbfe4bfb4fa
P1234567717 <79258405+P1234567717@users.noreply.github.com>
2021-03-17 10:49:32 +0530

    S
    S

--- Program-2.c
+++ Program-2.c
@@ -6,11 +6,11 @@
 #include<stdio.h>
 void main()
 {
-	int i=1,n;
-	printf("Enter upper limit : ");
-	scanf("%d",&n);
-	printf("The natural numbers are ");
-	for(i=1;i<=;i++)
-		printf(" ",i);
+  int i=1,n;
+  printf("Enter upper limit : ");
+  scanf("%d",&n);
+  printf("The natural numbers are ");
+  for(i=1;i<=;i++)
+     printf("%d\t",i);
 	//Fill the missing code here
  }
